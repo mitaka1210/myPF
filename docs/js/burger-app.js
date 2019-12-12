10e6baec6f4d77776daef8274d1431a10d5c9e61
Dimitar <dimitar_dimitrov12@mail.bg>
2019-12-12 21:31:39 +0200

    add contact form and animation:Name

--- docs/js/burger-app.js
+++ docs/js/burger-app.js
@@ -1,11 +1,7 @@
-window.onload=function() {
-  
-  var menuIcon = document.querySelector(".hamburger-menu");	
-  var navbar = document.querySelector(".navbar");	
-  
-  
-  menuIcon.addEventListener("click", () => {	
-    navbar.classList.toggle("change");	
-  });
+const menuIcon = document.querySelector(".hamburger-menu");	
+const navbar = document.querySelector(".navbar");	
 
-};
+
+menuIcon.addEventListener("click", () => {	
+  navbar.classList.toggle("change");	
+});
